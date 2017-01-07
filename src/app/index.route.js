7916cd83a2e1f51e99f323bf4dc1b02ac3826391
Leclaire1an <anjipu.m.e.leclaire@aims-senegal.org>
2017-01-07 17:48:37 +0000

    test &

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -14,6 +14,30 @@
         controller: 'MainController',
         controllerAs: 'main'
       });
+$stateProvider
+      .state('consumer', {
+        url: '/consumer',
+        templateUrl: 'app/main/api_consumer.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+$stateProvider
+      .state('Register', {
+        url: '/register',
+        templateUrl: 'app/main/api_register.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+$stateProvider
+      .state('token', {
+        url: '/token_request',
+        templateUrl: 'app/main/api_token_request.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+
+
+
 
     $urlRouterProvider.otherwise('/');
   }
